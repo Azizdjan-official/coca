cb12d420c14dea3d7c67787d57d2e7d93e06ae91
azizdjan_official <tulaganovazizjon@gmail.com>
2024-02-05 23:25:56 +0500

    a

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
-      this is header
+      <header className='w-[80%] mx-auto flex justify-between bg-slate-300 py-4'>
+        <h1>Coca </h1>
+        <div className='flex gap-10'>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/pricing">Pricing</Link>
+        </div>
+      </header>
     </div>
   )
 }
