00f58bfa4bd250acd48575880c1e2e84264cac7d
azizdjan_official <tulaganovazizjon@gmail.com>
2024-02-06 08:30:30 +0500

    header is done

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import HeaderIcon from './../../Icons/Header-icon';
 
 const Header = () => {
   return (
     <div>
-      <header className='w-[80%] mx-auto flex justify-between bg-slate-300 py-4'>
-        <h1>Coca </h1>
+      <header className='w-[90%] mx-auto  flex justify-between border py-4 items-center font-bold '>
+        <HeaderIcon/>
         <div className='flex gap-10'>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
