b1400e83d3ca570311555578d64b29edaeeee13f
Xondamir <abdulazizovxondamirxon@gmail.com>
2024-02-06 12:43:34 +0500

    pricing

--- src/Layout/Main-layout.jsx
+++ src/Layout/Main-layout.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import Header from "./Header/Header";
+import { Footer } from "./Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
-    return (
-        <div >
-          <div>
-            <Header />
-            <main>
-              <Outlet />
-            </main>
-          </div>
-          <Footer />
-        </div>
-      );
-}
+  return (
+    <div>
+      <div>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
--- src/Pages/Pricing/Pricing.jsx
+++ src/Pages/Pricing/Pricing.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import "./pricing.css";
 const Pricing = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div>
+        <div className="grid justify-center py-28">
+          <div className="rounded-full w-32 p-1 text-center bg-blue-500">
+            <p>Pricing Plans 🤑</p>
+          </div>
+        </div>
 
-export default Pricing
+        <div className="text-center">
+          <h1 className=" w-[40%] mx-auto font-medium  text-5xl">
+            Choose a plan for a more advanced business
+          </h1>
+          <p className="py-28 ">Annual pricing (save 20%)</p>
+        </div>
+        
+        <div className="flex justify-between">
+          <div className="showcase-img"></div>
+          <div className="showcase-img-2"></div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
